39de918a3c9d924842fbc15d382d7d6088b66920
Kev Zettler <kevzettler@gmail.com>
2020-04-16 15:44:36 -0700

    remove .only

--- test/basic.test.js
+++ test/basic.test.js
@@ -50,7 +50,7 @@ it('works', () => {
 });
 
 
-describe.only("Against a Cube", () => {
+describe("Against a Cube", () => {
   const c = cube(100);
   const tris = c.cells.map((tri) => {
     return [
@@ -91,33 +91,33 @@ describe.only("Against a Cube", () => {
   });
 
 
-    it.only('collides on Y- ', () => {
-      const end = [0, -200, 0];
+  it('collides on Y- ', () => {
+    const end = [0, -200, 0];
 
-      traceInfo.resetTrace(
-        [0, 200, 0],
-        end,
-        10
-      );
+    traceInfo.resetTrace(
+      [0, 200, 0],
+      end,
+      10
+    );
 
-      for(var i =0; i<tris.length; i++) {
-        var tri = tris[i];
-        traceSphereTriangle(tri[0], tri[1], tri[2], traceInfo);
-      }
+    for(var i =0; i<tris.length; i++) {
+      var tri = tris[i];
+      traceSphereTriangle(tri[0], tri[1], tri[2], traceInfo);
+    }
 
-      expect(traceInfo.intersectPoint[0]).toEqual(0);
-      expect(traceInfo.intersectPoint[1]).toEqual(50);
-      expect(traceInfo.intersectPoint[2]).toEqual(0);
+    expect(traceInfo.intersectPoint[0]).toEqual(0);
+    expect(traceInfo.intersectPoint[1]).toEqual(50);
+    expect(traceInfo.intersectPoint[2]).toEqual(0);
 
-      const traceEnd = traceInfo.getTraceEndpoint(end)
-      expect(traceEnd[0]).toEqual(0);
-      expect(traceEnd[1]).toEqual(60);
-      expect(traceEnd[2]).toEqual(0);
+    const traceEnd = traceInfo.getTraceEndpoint(end)
+    expect(traceEnd[0]).toEqual(0);
+    expect(traceEnd[1]).toEqual(60);
+    expect(traceEnd[2]).toEqual(0);
 
 
-      expect(traceInfo.getTraceDistance()).toEqual(140)
+    expect(traceInfo.getTraceDistance()).toEqual(140)
 
-      expect(traceInfo.collision).toBeTruthy();
+    expect(traceInfo.collision).toBeTruthy();
   });
 
 
